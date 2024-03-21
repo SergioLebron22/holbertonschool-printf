4713d44a1cfe5679ed4a5ac959f3a52993f32b8a
niniisg <nisselsanchez@gmail.com>
2024-03-21 16:05:12 -0400

    commiting before change

--- main.c
+++ main.c
@@ -7,7 +7,10 @@
  *
  * Return: Always 0
  */
-int main(void)
+
+
+
+int main
 {
     int len;
     int len2;
--- main.h
+++ main.h
@@ -1,13 +1,18 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <unistd.h>
+#include <stdarg.h>
+
 int _printf(const char *format, ...);
 
 typedef struct Format
 {
 char t;
 void (*f)(va_list args);
-} format;
+} flor;
 
 int isFormat(const char * const format);
+int _putchar(char c);
+
 #endif
