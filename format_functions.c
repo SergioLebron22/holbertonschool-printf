0241b8565de5bb088f6100ddc0595df71b822dec
SergioLebron22 <lebronsergio22@gmail.com>
2024-03-20 23:24:29 -0400

    Made changes to format functions file

--- format_functions.c
+++ format_functions.c
@@ -3,36 +3,52 @@
 #include "main.h"
 
 /**
+ * print_str - will print a string where %s is in printf
  *
+ * @args: string passed
  *
- *
+ * Return: number of characters in string
  */
 
-void print_str(va_list args)
+int print_str(va_list args)
 {
-	;
+	int i;
+	char *str = va_arg(args, char *);
+
+	if (str[i] == '\0')
+		str = "(null)";
+
+	for (i = 0; i != '\0'; i++)
+		_putchar(va_arg(args, char *));
+	return (i - 1);
 }
 
 /**
+ * print_char - will print a character where %c is in printf
  *
+ *@args: character passed
  *
- *
- *
+ * Return: 0
  */
 
 
-void print_char(va_list args)
+int print_char(va_list args)
 {
-	;
+	_putchar(va_arg(args, int));
+	return (0);
 }
 
 /**
+ * print_percent - will print the character % where %% is in printf
  *
+ * @args: format %%
  *
+ * Return: 0
  */
 
-void print_percent(va_list args)
+int print_percent(va_list args)
 {
-	;
+	_putchar('%');
+	return (0);
 }
 
